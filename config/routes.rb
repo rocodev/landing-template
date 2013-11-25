02a4107f88cf7538392e9627397ac06a0c8d1ea0
Teng-hao Chang (Zhusee) <zhusee2@gmail.com>
2013-11-25 17:02:45 +0800

    Fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Landing::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
-  get "/tours", :to => "pages#tour"
+  get "/tours", :to => "pages#tours"
   get "/pricing", :to => "pages#pricing"
   get "/explore", :to => "pages#explore"
 
